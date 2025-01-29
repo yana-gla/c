e8e711ba5145944b781cf572fbb1229615dcef26
yana.glazer <yana.glzr@gmail.com>
2025-01-29 18:20:45 +0200

    quiz

--- c/bits_rev/bitwise.c
+++ c/bits_rev/bitwise.c
@@ -1,12 +1,14 @@
+#include <stddef.h>
 #include <stdio.h>
+#include "bitwise.h"
 
-typedef unsigned char uchar_t;
-
+/*bit shifting is equal to muliply/ divide by 2*/
 long Pow2(unsigned int x, unsigned int y)
 {
-    return (long)(x << y);
+    return ((long)(x << y));
 }
 
+/*number that is power of 2, includes only 1 - 1*/
 int IsPow2Lp(unsigned int n)
 {
     /*loop*/
@@ -19,13 +21,13 @@ int IsPow2Lp(unsigned int n)
         n >>= 1;
     }
     
-    return 1;
+    return (1);
 }
 
+/*without loop*/
 int IsPow2WLp(unsigned int n)
 {
-    /*without loop*/
-    return (0 != n && (0 == (n-1) & n));
+    return (0 != n && (0 == ((n-1) & n)));
 }  
 
 int Ads1(int num)
@@ -78,7 +80,7 @@ uchar_t ByteMirror1(uchar_t byte)
     {
          mir_byte <<= 1;
          lsb =  (1 & byte); /*looking on lsb of byte, if 1 ->1, if 0 -> 0*/ 
-         mir_byte = mir_byte | lsb; 
+         mir_byte |= lsb; 
          byte >>= 1;
     }
     
@@ -155,6 +157,7 @@ unsigned int Clsst16(const unsigned int num)
 
 void Swap(int *x, int *y)
 {
+    
     *x = *x ^ *y;
     *y = *x ^ *y;
     *x = *x ^ *y;
@@ -167,7 +170,6 @@ uchar_t Cnt1BitsLp(unsigned int num)
     while (num > 0)
     {
         count += (1 & num);
-        
         num >>= 1;
     }
     
@@ -184,7 +186,7 @@ uchar_t Cnt1BitsWLp(unsigned int i)
      return  i >> 24;               /* return just that top byte (after truncating to 32-bit even when int is wider than uint32_t)*/
 }
 
-/*TO FIX*/
+
 /*A float that's cast to an int causes the fractional part to be truncated*/
 void PrntFltBts1(float num)
 {
@@ -194,21 +196,20 @@ void PrntFltBts1(float num)
         num = (int)num >> 1;
     }
         printf("\n");
+}
 
-/*TO FIX
 /*pointer with same number of bytes int = float 32*/
-void PrntFltBts2(float *fp)
+void PrntFltBts2(float fp)
 {
-
     int i = 0;
     
-    // float as an integer pointer cast
-    unsigned int* ip = (unsigned int*)fp;
-    unsigned int bits = *ip; /*the value*/
+    /* float as an integer pointer cast*/
+    unsigned int *ip = (unsigned int*)&fp;
+    unsigned int int_val = *ip; /*the value*/
 
     for (i = 31 ; 0 <= i; --i)
     {
-        printf("%d", (bits >> i) & 1);
+        printf("%d", (int_val >> i) & 1);
 
     }
     printf("\n");
@@ -216,48 +217,5 @@ void PrntFltBts2(float *fp)
 
 
 
-
-int main()
-{
-      unsigned int arr[] = {0, 7, 11, 14, 15};
-      unsigned int x = -2;
-      uchar_t uchr = 1;
-      int y = 5, z = 10;
-      int *py = &y, *pz = &z;
-      float f = 65.125;
-      
-      
-      printf("Answer Question 1 is %u, %u, %u\n", x, x<<2, x>>2);
-
-      printf("Answer Exercises 1 is %ld\n", Pow2(2,0));
-      printf("Answer Exercises 2 loop is %d\n", IsPow2Lp(0));
-      printf("Answer Exercises 2 w.loop is %d\n\n", IsPow2WLp(0));
-      
-      printf("Answer Exercises 3 5+1 is %d\n", Ads1(5));
-      printf("Answer Exercises 4 is ");
-      Prnt3Bits(arr, 5);
-      printf("\n");
-      
-      printf ("Answer ByteMirror1 is %u\n", ByteMirror1(uchr));
-      printf ("Answer ByteMirror2 is %u\n", ByteMirror2(uchr));
-      
-      printf ("Answer Chk2_26Bit for 99 (01100011) is %d\n", Chk2_26Bit(99));
-      printf("Answer Chk1_26Bit for 3 (00000011) is %d\n",Chk1_26Bit(3));
-      printf("Answer Swap_35Bit1 for 4 (0000 0100) is 16 (0001 0000) %u\n",Swap_35Bit1(4));
-      printf("Answer Swap_35Bit2 for 4 (0000 0100) is 16 (0001 0000) %u\n\n",Swap_35Bit2(4));
-      
-      printf ("Answer Clsst16 for 22 is %d\n", Clsst16(16));
-      
-      printf ("Before the swap y = 5, z = 10\n"); 
-      Swap(py, pz);
-      printf ("After the swap y = %d, z =%d This is Cool!\n\n", y, z);
-      printf ("Number of On bits in 1010 1010 (170) is (loop) %d \n", Cnt1BitsLp(170));
-      printf ("Number of On bits in 1010 1010 (170) is (wloop) %d \n", Cnt1BitsWLp(170));
-      
-      PrntFltBts1(65.125);
-      /*PrntFltBts2(&f);*/
-      
-    return ;		
-}
 
 

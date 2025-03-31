edce2591d3f4bb82611719faf1811bd81eaca9e6
yana.glazer <yana.glzr@gmail.com>
2025-03-31 15:11:52 +0300

    knight

--- ds/include/knight_tour.h
+++ ds/include/knight_tour.h
@@ -2,11 +2,9 @@
 * File: knight_tour.h
 *
 * Purpose:
-*   Implement
+*   Implement Knight’s tour problem
 *	
 *	
-*	
-*
 * Author:
 *   Yana Glazer
 * Code reviewd by:
@@ -18,11 +16,9 @@
 #ifndef __KNIGHT_TOUR_H__
 #define __KNIGHT_TOUR_H__
 
-
 #define BOARD_DIMENSIONS (8)
 #define BOARD_SIZE (BOARD_DIMENSIONS * BOARD_DIMENSIONS)
 
-/* Two Options for position_t */
 typedef unsigned char position_t; /*x: 11110000 y: 00001111*/
 typedef unsigned char uchar_t;
 
@@ -33,34 +29,14 @@ typedef enum
     TIMEOUT  /* timed-out before finding solution */
 } status_t;
 
-/* returns success / failure indicator */
-status_t KnightTour(
-                position_t initial_position, 
-                position_t path[BOARD_SIZE],    /* path arr to store results */
-                unsigned int max_runtime,       /* allowed runtime duration before TIMEOUT */
-                int use_heuristics);            /* flag to apply Warnsdorff´s heuristics: 1 = use it*/
 
+status_t KnightTour(position_t initial_position, 
+            	    position_t path[BOARD_SIZE],  
+             	   unsigned int max_runtime,        
+               		 int use_heuristics);         
 
 position_t BuildPosition(unsigned char x, unsigned char y);
 void ExtractPosition(position_t pos, unsigned char* x, unsigned char* y);
-
-
-/* Client Side *************************************************************/
-/* position 0 will be the initial position we get from the user */
-position_t path[64] = {0};
-
-
-/* Other Notes *************************************************************/
-/**
- * inside function SolveKnightTour:
- * (initial positions are in path[0])
- * positions[0].x = initial_x;
- * positions[0].y = initial_y; 
- */
- 
- /* reach solution in 5 minutes max, even if it can lead to success. */
-
-
 
 
 #endif
